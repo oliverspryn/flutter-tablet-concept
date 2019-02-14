76c54045cf18e63efdcdcf68e4a64eddd600626e
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-02-14 10:57:30 -0500

    Create the project

---
+++ android/app/src/main/kotlin/com/upmc/enterprises/fluttertabletconcept/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.upmc.enterprises.fluttertabletconcept
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
